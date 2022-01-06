21f2d9f5d7f1b6057e9f5b88348f955da30fd73c
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2022-01-06 14:49:26 +0200

    Added caches for moving and titling the progress bar

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -22,6 +22,8 @@ pub struct ProgressBar {
     bar: Vec<u8>,
     title: String,
     row: usize,
+    prefix: Vec<u8>,
+    suffix: Vec<u8>,
     out: Stdout,
 }
 impl ProgressBar {
@@ -29,15 +31,19 @@ impl ProgressBar {
     /// based on update count relative to `source_len`
     pub fn new(source_len: usize, bar_len: usize) -> Self {
         // A bar's length is the length of content not '\r', '[' or ']'
-        let bar : Vec<u8> = iter::repeat('.' as u8)
-            .take(bar_len)
-            .collect();
+        let bar: Vec<u8> = iter::repeat('.' as u8).take(bar_len).collect();
+        let title = String::from("Progress");
+        let row = 0;
+        let prefix = Self::prefix_from(row, &title);
+
         ProgressBar {
             source_progress: 0,
             threshold: source_len / bar_len,
             bar,
-            title: String::from("Progress"),
-            row: 0,
+            title,
+            row,
+            prefix,
+            suffix: Self::suffix_from(row),
             out: io::stdout(),
         }
     }
@@ -56,11 +62,16 @@ impl ProgressBar {
     /// Change the title shown next to the progress bar
     pub fn title(&mut self, title: &str) {
         self.title = String::from(title);
+        // Cache the new prefix now that title has changed
+        self.prefix = Self::prefix_from(self.row, &self.title);
     }
 
     /// Change the row-offset of the progress bar
     pub fn row(&mut self, row: usize) {
         self.row = row;
+        // Cache the new prefix and suffix now that row-offset has changed
+        self.prefix = Self::prefix_from(self.row, &self.title);
+        self.suffix = Self::suffix_from(self.row);
     }
 
     fn update(&mut self) -> bool {
@@ -78,31 +89,21 @@ impl ProgressBar {
 
     /// Print based on terminal row on which to position the bar.
     fn print(&mut self) -> IOResult<()> {
-        // Set how much the cursor will be moved with escape sequences
-        let move_rows = format!("\x1b[{}", self.row);
-        // Do a little dance, make a little love
-        let prefix = {
-            let mut temp = Vec::from(move_rows.as_bytes());
-            // 'B' => Move down n lines
-            temp.extend(b"B\r");
-            temp.extend(self.title.as_bytes());
-            temp.extend(b" [");
-            temp
-        };
-
-        // Flag to choose the right suffix based on the bar being full or not
+        // Flag to choose the right ending based on the bar being full or not
         let bar_progress = self.bar.iter()
             .rposition(|x| *x == '=' as u8)
             .unwrap_or(0) + 1;
         let bar_full = self.bar.len() <= bar_progress;
 
-        // Create a vec for the bytes to be written
-        let mut bytes = vec![];
-                   // bar            + prefix       +"] Done!"
-        bytes.reserve(self.bar.len() + prefix.len() + 7);
+        // Create a fitting vec for the bytes to be written
+        let mut bytes = Vec::with_capacity(
+            // bar         + prefix            +"] Done!"
+            self.bar.len() + self.prefix.len() + 7
+        );
 
-        // Add the correct bytes in order
-        bytes.extend(prefix);
+        // Add the correct bytes in order:
+        bytes.extend(&self.prefix);
+
         bytes.extend(self.bar.as_slice());
         if bar_full {
             bytes.extend(b"] Done!");
@@ -110,13 +111,23 @@ impl ProgressBar {
             bytes.extend(b"]")
         }
 
-        // Move back up ('A') to the row where started
-        bytes.extend(format!("{}A", move_rows).as_bytes());
+        bytes.extend(&self.suffix);
 
         // Write into stdout
         // NOTE Stdout needs to be locked for the write operations
         let mut handle = self.out.lock();
         handle.write(bytes.as_slice())?;
         handle.flush()
+    }
+
+    /// Create a string that will move the cursor down the amount of rows
+    fn prefix_from(row: usize, title: &str) -> Vec<u8> {
+                        // 'B' => Move down n lines
+        Vec::from(format!("\x1b[{}B\r{} [", row, title).as_bytes())
+    }
+
+    fn suffix_from(row: usize) -> Vec<u8> {
+        // Move back up ('A') to the row where started
+        Vec::from(format!("\x1b[{}A", row).as_bytes())
     }
 }
