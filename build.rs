6dbeae8d53d5235763de5461622cb210e43914f0
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2024-07-18 21:06:31 +0300

    Overengineer even the smallest of build scripts

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
 fn main() {
+    const OUTDIR: &str = "./target";
+    let outobj = format!("{}/{}", OUTDIR, "cread.o");
+    let outlib = format!("{}/{}", OUTDIR, "libcread.a");
     println!("cargo::rerun-if-changed=cread.c");
     std::process::Command::new("gcc")
-        .args(["-c", "-o", "cread.o", "cread.c"])
+        .args(["-c", "-o", &outobj, "cread.c"])
         .output()
         .unwrap();
     std::process::Command::new("ar")
-        .args(["-rcs", "libcread.a", "cread.o"])
+        .args(["-rcs", &outlib, &outobj])
         .output()
         .unwrap();
-    println!("cargo::rustc-link-search=.");
-    println!("cargo:rustc-link-lib=cread");
+    println!("cargo::rustc-link-search={}", OUTDIR);
+    println!("cargo:rustc-link-lib={}", outlib.strip_prefix(&format!("{}/lib", OUTDIR)).unwrap().strip_suffix(".a").unwrap());
 }
