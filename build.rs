57cec14fb01d475a8795f712524e1314ae09fbe2
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2024-10-05 12:33:30 +0300

    (Maybe) Enable dependants to correctly build the C-code with cargo

--- build.rs
+++ build.rs
@@ -1,37 +1,18 @@
-use std::fs;
-use std::io;
+use std::env;
+use std::path::Path;
 
 fn main() {
-    println!("cargo::rerun-if-changed=cread.c");
-    const OUTDIR: &str = "./target";
-    let outobj = format!("{}/{}", OUTDIR, "cread.o");
-    let outlib = format!("{}/{}", OUTDIR, "libcread.a");
-
-    // Remove previous build.
-    for path in vec![&outobj, &outlib] {
-        match fs::remove_file(path) {
-            Err(error) => match error.kind() {
-                io::ErrorKind::PermissionDenied => {
-                    eprintln!("Not allowed to remove old file: {}", path);
-                    std::process::exit(1);
-                },
-                io::ErrorKind::NotFound => {
-                    eprintln!("File not found: {}", path);
-                },
-                other_kind => panic!("Should not happen: {}", other_kind)
-            },
-            _ => {}
-        }
-    }
-
+    let outdir = env::var("OUT_DIR").unwrap();
     std::process::Command::new("gcc")
-        .args(["-c", "-o", &outobj, "cread.c"])
-        .output()
+        .args(["-c", "-o", &format!("{}/cread.o", outdir), "cread.c"])
+        .status()
         .unwrap();
     std::process::Command::new("ar")
-        .args(["-rcs", &outlib, &outobj])
-        .output()
+        .args(["crs", "libcread.a", "cread.o"])
+        .current_dir(&Path::new(&outdir))
+        .status()
         .unwrap();
-    println!("cargo::rustc-link-search={}", OUTDIR);
-    println!("cargo::rustc-link-lib={}", outlib.strip_prefix(&format!("{}/lib", OUTDIR)).unwrap().strip_suffix(".a").unwrap());
+    println!("cargo::rustc-link-search=native={}", outdir);
+    println!("cargo::rustc-link-lib=static=cread");
+    println!("cargo::rerun-if-changed=cread.c");
 }
